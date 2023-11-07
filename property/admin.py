42c5ca1d092e775c3c7e07cd410c2e6b7a41edec
AlexWoIf <alexey.barannikov@gmail.com>
2023-11-08 01:58:43 +0300

    add db model for complaints

--- property/admin.py
+++ property/admin.py
@@ -1,14 +1,19 @@
 from django.contrib import admin
 
-from .models import Flat
+from .models import Flat, Complaint
 
 
 class FlatAdmin(admin.ModelAdmin):
     search_fields = ('town', 'address', 'owner', )
-    readonly_fields = ["created_at"]
+    readonly_fields = ["created_at", ]
     list_display = ('address', 'price', 'new_building', 'construction_year', )
     list_editable = ('new_building', )
     list_filter = ('new_building', 'rooms_number', 'has_balcony', )
 
 
+class ComplaintAdmin(admin.ModelAdmin):
+    raw_id_fields = ('flat', )
+
+
 admin.site.register(Flat, FlatAdmin)
+admin.site.register(Complaint, ComplaintAdmin)
